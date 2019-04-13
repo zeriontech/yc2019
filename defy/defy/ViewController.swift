255dbc7ae7f6e7776c0844b3daeb5dcd987f21d5
Evgeny Yurtaev <eugene@yurtaev.me>
2019-04-12 21:24:54 -0700

    Add Plaid library

--- defy/defy/ViewController.swift
+++ defy/defy/ViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 import Bitski
 import Web3
 import PromiseKit
-
-let BitskiClientID = "6e244309-4f5f-44cf-a72f-bc3e1c19ad52"
-let BitskiRedirectURL = "io.zerion.defy://application/callback"
+import LinkKit
 
 class ViewController: UIViewController {
     
@@ -31,17 +29,58 @@ class ViewController: UIViewController {
             self.web3 = Bitski.shared?.getWeb3()
             self.getAccount()
         }
+        
+//        if Bitski.shared?.isLoggedIn == true {
+//            self.web3 = Bitski.shared?.getWeb3()
+//            // show logged in state
+//        } else {
+//            // show logged out state
+//        }
     }
 
     func getAccount() {
         if let web3 = self.web3 {
             firstly {
                 web3.eth.accounts().firstValue
-            }.done { account in
+            }.done { [weak self] account in
                 print(account.hex(eip55: true))
+                // With shared configuration from Info.plist
+                if let self = self {
+                    let linkViewDelegate = self
+                    let linkViewController = PLKPlaidLinkViewController(delegate: linkViewDelegate)
+                    if (UI_USER_INTERFACE_IDIOM() == .pad) {
+                        linkViewController.modalPresentationStyle = .formSheet;
+                    }
+                    self.present(linkViewController, animated: true)
+                }
             }
         }
-        
     }
 }
 
+extension ViewController: PLKPlaidLinkViewDelegate {
+    func linkViewController(_ linkViewController:
+        PLKPlaidLinkViewController, didSucceedWithPublicToken publicToken:
+        String, metadata: [String : Any]?) {
+        dismiss(animated: true) {
+            // Handle success, e.g. by storing publicToken with your service
+            NSLog("Successfully linked account!\npublicToken: (publicToken)\nmetadata: (metadata ?? [:])")
+//                self.handleSuccessWithToken(publicToken, metadata: metadata)
+        }
+    }
+    
+    func linkViewController(_ linkViewController:
+        PLKPlaidLinkViewController, didExitWithError error: Error?,
+                                    metadata: [String : Any]?) {
+        dismiss(animated: true) {
+            if let error = error {
+                NSLog("Failed to link account due to: (error.localizedDescription)\nmetadata: (metadata ?? [:])")
+//                self.handleError(error, metadata: metadata)
+            }
+            else {
+                NSLog("Plaid link exited with metadata: (metadata ?? [:])")
+//                self.handleExitWithMetadata(metadata)
+            }
+        }
+    }
+}
